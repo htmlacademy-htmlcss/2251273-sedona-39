b49efca72daeacc6c47565831ee3269fe1db3f2f
echenko <echenko.v.g@yandex.ru>
2024-03-04 08:55:26 +0300

    Добавляет скрипт

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,7 @@
-let header_button = document.querySelector("header_button");
+let header_button = document.querySelector(".header-button");
+let modal_container = document.querySelector(".modal-container");
+let modal_button_close = document.querySelector(".modal-button-close");
 
-header_button.on
+
+header_button.onclick = () => modal_container.classList.toggle("modal-container-closed");
+modal_button_close.onclick = () => modal_container.classList.toggle("modal-container-closed");
