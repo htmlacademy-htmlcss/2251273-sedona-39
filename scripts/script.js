dc18891c4d3c3d0153b6cd0745b56e62a83f76af
echenko <echenko.v.g@yandex.ru>
2024-03-13 19:33:59 +0300

    Устраняет замечания

--- scripts/script.js
+++
@@ -1,7 +0,0 @@
-let header_button = document.querySelector(".header-button");
-let modal_container = document.querySelector(".modal-container");
-let modal_button_close = document.querySelector(".modal-button-close");
-
-
-header_button.onclick = () => modal_container.classList.toggle("modal-container-closed");
-modal_button_close.onclick = () => modal_container.classList.toggle("modal-container-closed");
